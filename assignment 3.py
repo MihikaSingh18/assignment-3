0708953320da31d18129f6e441657be4771f6d90
MihikaSingh18 <138354730+MihikaSingh18@users.noreply.github.com>
2023-07-18 20:43:45 +0530

    Add files via upload

---
+++ assignment 3.py
@@ -0,0 +1,27 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      singh
+#
+# Created:     18-07-2023
+# Copyright:   (c) singh 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+
+i=5
+while(i>=1):
+    print(i * "*")
+    i=i-1
+
+friends = ["amar" , "akbar" , "anthony"]
+print(friends[0])
+print(friends[1])
+print(friends[-1])
+print(friends[-2])
+print(friends)
+print(friends[0:1])
+print(friends[0:2])
+print(friends[0:3])
